8b359a272b28c312e1a28f5da65166a4c64758f8
masnoonjunaid <masnoon20@gmail.com>
2025-05-21 13:00:40 +0530

    my profile links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -169,6 +169,53 @@ export default function Home() {
       </table>
     </div>
       </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://masnoonjunaid.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Portfolio
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://github.com/masnoonjunaid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Github
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://linkedin.com/in/masnoon-junaid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Linkedin →
+        </a>
+      </footer>
     </div>
   );
 }
